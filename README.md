2f74276652f0f902dde05b2ba670614bc1700657
Aniii002 <118073443+anii002@users.noreply.github.com>
2023-06-03 21:46:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MERN Project(Rest-API LogIn Authentication with JWT)
+ o use Login and Register form with token Authentication in Material-UI. 
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <p>Welcome to MERN</p>
+        </>
+    );
+}
+
+export default Home;
